b1688443b16fa79345340f272ac0a7f818a701df
Ian <ianmcm.89@gmail.com>
2021-09-21 18:27:44 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(array) {
+    return array.map(a => createEmployeeRecord(a));
+}
+
+function createTimeInEvent(dateStamp) {
+    let newEvent = {
+        type: 'TimeIn',
+        date: dateStamp.substr(0, 10),
+        hour: parseInt(dateStamp.substr(11, 14), 10)
+    }
+    this.timeInEvents.push(newEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    let newEvent = {
+        type: 'TimeOut',
+        date: dateStamp.substr(0, 10),
+        hour: parseInt(dateStamp.substr(11, 14), 10)
+    }
+    this.timeOutEvents.push(newEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    let filterTimeIn = this.timeInEvents.filter(event => event.date === dateStamp);
+    let filterTimeOut = this.timeOutEvents.filter(event => event.date === dateStamp);
+    return (filterTimeOut[0].hour - filterTimeIn[0].hour)/100;
+}
+
+//console.log(hoursWorkedOnDate.call(updatedBpRecord, '2014-02-28'));
+
+function wagesEarnedOnDate(dateStamp) {
+    return this.payPerHour * (hoursWorkedOnDate.call(this, dateStamp));
+}
+
+//console.log(wagesEarnedOnDate.call(updatedBpRecord, '2014-02-28'));
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -13,11 +62,36 @@ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
+    //console.log(eligibleDates);
 
     const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
+        //console.log(memo);
+        //console.log(d);
+        return memo + wagesEarnedOnDate.call(this, d);
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }
 
+//console.log(allWagesFor.call(updatedBpRecord));
+
+let src = [
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150]
+  ]
+let emps = createEmployeeRecords(src)
+let loki = findEmployeeByFirstName(emps, "Loki")
+
+function findEmployeeByFirstName(collection, firstNameString) {
+    let result = collection.filter(e => e.firstName === firstNameString);
+    return result[0];
+}
+
+//console.log(loki);
+
+function calculatePayroll(employees) {
+    let allWages = employees.map(e => allWagesFor.call(e));
+    //console.log(allWages);
+    let reducer = (previousValue, currentValue) => previousValue + currentValue;
+    return allWages.reduce(reducer);
+}
